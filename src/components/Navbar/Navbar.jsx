e9722a85c1c6a9da69b719d9f1b92b8d0db24892
clairemuiru1 <muiruclaire@gmail.com>
2024-09-07 17:51:34 +0300

    "attempting web responsiveness 1"

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import "./navbar.css"
+import React, { useState } from 'react';
+import "./navbar.css";
 
 function Navbar() {
-  
+  const [menuActive, setMenuActive] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuActive(!menuActive);
+  };
+
   return (
     <div className='navbar'>
       <h1>Claire Muiru</h1>
-      <ul className="navbar-menu">
+      <ul className={`navbar-menu ${menuActive ? 'active' : ''}`}>
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#skills">Skills</a></li>
@@ -17,8 +22,11 @@ function Navbar() {
       <div className="navbar-right">
         <button>Hire me</button>
       </div>
+      <div className="navbar-toggle" onClick={toggleMenu}>
+        ☰
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
